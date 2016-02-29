fab346dde3983722fdbb0a361d0a8eb17fa5ccdf
jakano <jakano@gatech.edu>
2016-02-29 18:31:06 -0500

    Test

--- app/src/main/java/gitmad/bitter/data/firebase/FirebaseCategoryProvider.java
+++ app/src/main/java/gitmad/bitter/data/firebase/FirebaseCategoryProvider.java
@@ -1,13 +1,31 @@
 package gitmad.bitter.data.firebase;
 
+import com.firebase.client.Firebase;
+
+import java.util.concurrent.CountDownLatch;
+
 import gitmad.bitter.data.CategoryProvider;
+
 
 /**
  * Created by brian on 2/22/16.
  */
 public class FirebaseCategoryProvider implements CategoryProvider {
+    private Firebase catproRef;
+    public FirebaseCategoryProvider() {
+        catproRef = new Firebase("https://bitter-gitmad.firebaseio.com/postCategories");
+    }
+
+    private void awaitLatch(CountDownLatch latch) {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public String[] getCategories() {
-        return new String[0];
+        return catproRef;
     }
 }
